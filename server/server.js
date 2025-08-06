996a170627146e315a3d375204ee0e07e68c13a8
Arkopravob <arka.bhuin@gmail.com>
2025-08-06 13:14:14 +0530

    Fix: enable ES module + remove mongoose deprecations

--- server/server.js
+++ server/server.js
@@ -16,10 +16,8 @@ import leadRoutes from './routes/leadRoutes.js';
 app.use('/api/leads', leadRoutes);
 
 // DB + Server Init
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('MongoDB connected');
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    }).catch((err) => console.log(err));
